427ec3b534a1e89d7c5d19ebc0e137c89b41e970
Cory Hedeen <hedeencory@gmail.com>
2019-07-26 12:09:34 -0400

    Write character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  return (
+    <div className="character-card">
+      <span>{props.character.name}</span>
+      <span>{props.character.status}</span>
+      <span>{props.character.species}</span>
+      <span>{props.character.type}</span>
+    </div>
+  )
 }
