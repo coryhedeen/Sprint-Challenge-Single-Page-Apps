c5a842d439a3604174f2c0010099f87de75e3bdb
Cory Hedeen <hedeencory@gmail.com>
2019-07-26 12:08:35 -0400

    Write card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function LocationCard ({ name, type, dimension, residents }) {
   // image={image}
-  return (<span>todo: location</span>
+  return (
+    <div className="locationCard">
+      <span>{name}</span>
+      <span>{type}</span>
+      <span>{dimension}</span>
+      <span>{residents}</span>
+    </div>
   )
 }
