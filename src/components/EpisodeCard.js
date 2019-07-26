fae736b513e5aa780534c3b4ad39f9f8db2cccaa
Cory Hedeen <hedeencory@gmail.com>
2019-07-26 19:45:21 -0400

    Style

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -5,9 +5,11 @@ export default function EpisodeCard(props) {
   return (
     <div className="card">
       <h2>{props.episodes.name}</h2>
+      <div className="info2">
       <h3>Aired: {props.episodes.air_date}</h3>
       <h3>{props.episodes.episode}</h3>
       <h3>Characters Involved: {props.episodes.characters.length}</h3>
+      </div>
     </div>
   )
 }
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -5,9 +5,11 @@ export default function LocationCard (props) {
   return (
     <div className="card">
       <h2>{props.location.name}</h2>
-      <h3>Type: {props.location.type}</h3>
-      <h3>Dimension: {props.location.dimension}</h3>
-      <h3>Residents: {props.location.residents.length}</h3>
+      <div className="info2">
+        <h3>Type: {props.location.type}</h3>
+        <h3>Dimension: {props.location.dimension}</h3>
+        <h3>Residents: {props.location.residents.length}</h3>
+      </div>
     </div>
   )
 }
