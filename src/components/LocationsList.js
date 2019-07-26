00638c7f824bfda6f109311c7ce810c1ac614af2
Cory Hedeen <hedeencory@gmail.com>
2019-07-26 12:09:11 -0400

    Write axios request and map card

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import LocationCard from './LocationCard.js';
+import axios from 'axios';
 
-export default function LocationsList() {
+export default function LocationsList(props) {
+  const [location, setLocation] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/locations/")
+      .then(res => { return console.log(res.data.results)
+            setLocation(res.data.results)})
+      .catch(err => console.log(err))
+  }, [location])
 
+  return (
+    <section className='character-list grid-view'>
+      <h2>{location.map(item => {
+        return <LocationCard location={item}/>
+      })}</h2>
+    </section>
+)
 }
